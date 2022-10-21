473178b8b4335c714fe7c398e1e2675642ff1a0a
Ayush Rai <ayushayushravi@gmail.com>
2022-10-21 12:15:51 +0530

    first commit

---
+++ Mustache.js
@@ -0,0 +1,11 @@
+var inputd = document.getElementById("date");
+var inputL = document.getElementById("locality");
+var inputT = document.getElementById("title");
+var inputD = document.getElementById("description");
+
+
+ 
+  inputd.setAttribute('value', '22-10-2022');
+  inputL.setAttribute('value', '22 .1');
+  inputT.setAttribute('value', 'Delhi');
+  inputD.setAttribute('value', 'Capital of india');
